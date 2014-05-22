753e6cc4c8e88f263ff68f35d4d63c67a99d4710
Oskar Szrajer <oskarszrajer@gmail.com>
2014-05-22 12:48:42 +0200

    fix extra end

--- spec/unit/main_spec.rb
+++ spec/unit/main_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../../lib/main'
-# Mateusz
+
 describe 'Main' do
+
   it 'should initialize(with  no params)' do
     expect {
-      Main.new()
+      Main.new
     }.not_to raise_error
   end
 
@@ -13,16 +14,17 @@ describe 'Main' do
         Main.new.command("x")
       }.to raise_error(WrongInput)
     end
+
     it 'accepts commands in [uldr] format' do
       expect{
         Main.new.command("u")
       }.not_to raise_error
     end
+
     it 'accepts case insensitive commands in [uldr] format' do
       expect{
         Main.new.command("U")
       }.not_to raise_error
     end
   end
-end
 end
