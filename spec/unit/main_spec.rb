dfcddc582e4c2cbbc6b23dcd440452a93903dd0a
Jacek Możejko <j.mozejko@gmail.com>
2014-05-26 10:13:39 +0200

    Store command for processing

--- spec/unit/main_spec.rb
+++ spec/unit/main_spec.rb
@@ -8,48 +8,64 @@ describe 'Main' do
   end
 
   describe '#command' do
+    let(:main) { Main.new }
+
     it 'does not accept commands not in [uldr] digit pattern' do
       expect{
-        Main.new.command('foo')
+        main.command('foo')
       }.to raise_error(WrongInput)
     end
 
     it 'quits with "q" command' do
       expect{
-        Main.new.command("q")
+        main.command("q")
       }.to raise_error SystemExit
     end
 
     it 'quits with "Q" command' do
       expect{
-        Main.new.command("Q")
+        main.command("Q")
       }.to raise_error SystemExit
     end
 
     it 'accepts commands in [uldr] format' do
       expect{
-        Main.new.command("u")
+        main.command("u")
       }.not_to raise_error
     end
 
     it 'accepts case insensitive commands in [uldr] format' do
       expect{
-        Main.new.command("U")
+        main.command("U")
       }.not_to raise_error
     end
 
     # Jacek
     it 'accepts commands with a digit after a letter' do
       expect{
-        Main.new.command("u 3")
+        main.command("u 3")
       }.not_to raise_error
     end
 
     # Jacek
     it 'does not accept commands with negative numbers' do
       expect{
-        Main.new.command("d -7")
-      }.to raise(WrongInput)
+        main.command("d -7")
+      }.to raise_error WrongInput
+    end
+
+    before { main.command("d 2") }
+
+    it 'stores a valid command for internal processing' do
+      expect(
+        main.instance_variable_get(:@command)
+      ).not_to eq nil
+    end
+
+    it 'splits the command into processable chunks' do
+      expect(
+        main.instance_variable_get(:@command)
+      ).to eq ["d", "2"]
     end
   end
 end
